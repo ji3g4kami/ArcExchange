35da87cfa2ad1a5c8e5d65c0c7d1d3f6c58ff9df
Deng-Li Wu <ji3g4kami@gmail.com>
2026-05-09 12:49:06 +0100

    Initial Commit

---
+++ ArcExchange/ArcExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcExchangeApp.swift
+//  ArcExchange
+//
+//  Created by Deng-Li Wu on 09/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArcExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArcExchangeTests/ArcExchangeTests.swift
@@ -0,0 +1,19 @@
+//
+//  ArcExchangeTests.swift
+//  ArcExchangeTests
+//
+//  Created by Deng-Li Wu on 09/05/2026.
+//
+
+import Testing
+@testable import ArcExchange
+
+struct ArcExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
